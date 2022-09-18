48a53b4b9e57af950349006b0881363703055f43
Margarita <bano4kaa@mail.ru>
2022-09-18 23:04:12 +0300

    case 64 done

--- Case 64/Program.cs
+++ Case 64/Program.cs
@@ -1,3 +1,14 @@
 ﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
 // M = 1; N = 5. -> ""1, 2, 3, 4, 5""
 // M = 4; N = 8. -> ""4, 6, 7, 8""
+
+int m = 1;
+int n = 10;
+
+string Integer(int m, int n)
+    {
+        if (n==m-1) return string.Empty;
+       return $"{m} {Integer(m+1,n)} ";
+        
+    }
+Console.WriteLine(Integer(m,n));
