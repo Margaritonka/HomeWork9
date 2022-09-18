cd36d879aecfb3ddbca3ddb90d2bdf401e0409d0
Margarita <bano4kaa@mail.ru>
2022-09-18 23:11:44 +0300

    case 66 done

--- Case 66/Program.cs
+++ Case 66/Program.cs
@@ -1,3 +1,14 @@
 ﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+int m = 1;
+int n = 5;
+
+int Integer(int m, int n)
+    {
+        if (m==n+1) return 0;
+       return m+Integer(m+1,n);
+        
+    }
+Console.WriteLine(Integer(m,n));
